6bd2bcc1b4189aad40c9d5f7f7e9e0d864589152
Sebastian Scholl <sebscholl@gmail.com>
2020-12-04 17:02:57 -0500

    dependency updates

--- src/Application.js
+++ src/Application.js
@@ -1,47 +1,47 @@
-import React from 'react';
-import { AppProvider } from '@8base/react-sdk';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import { AppProvider } from '@8base/react-sdk'
+import { BrowserRouter } from 'react-router-dom'
 
-import Routes from './routes';
-import AuthClient from './shared/auth';
+import Routes from './routes'
+import AuthClient from './shared/auth'
 
-const workspaceEndpoint = process.env.REACT_APP_WORKSPACE_ENDPOINT;
+const workspaceEndpoint = process.env.REACT_APP_WORKSPACE_ENDPOINT
 
 class Application extends React.PureComponent {
   onRequestSuccess = ({ operation }) => {
-    const message = operation.getContext();
+    const message = operation.getContext()
 
     if (message) {
       // eslint-disable-next-line no-console
-      console.error(message);
+      console.error(message)
     }
-  };
+  }
 
   onRequestError = ({ graphQLErrors }) => {
-    const hasGraphQLErrors = Array.isArray(graphQLErrors) && graphQLErrors.length > 0;
+    const hasGraphQLErrors = Array.isArray(graphQLErrors) && graphQLErrors.length > 0
 
     if (hasGraphQLErrors) {
       graphQLErrors.forEach(error => {
         // eslint-disable-next-line no-console
-        console.error(error.message);
-      });
+        console.error(error.message)
+      })
     }
-  };
+  }
 
-  render() {
+  render () {
     return (
       <BrowserRouter>
         <AppProvider
           uri={workspaceEndpoint}
           authClient={AuthClient}
-          onRequestSuccess={this.onRequestSuccess}
           onRequestError={this.onRequestError}
+          onRequestSuccess={this.onRequestSuccess}
         >
           <Routes />
         </AppProvider>
       </BrowserRouter>
-    );
+    )
   }
 }
 
-export { Application };
+export { Application }
